9c25a06dfeccf61dbf95d3d4efbaf52eee444851
Pavle Subotic <psubotic@gmail.com>
2026-04-01 20:32:59 +0200

    fixed tests failing

--- src/detection/__init__.py
+++ src/detection/__init__.py
@@ -1,15 +1,1 @@
-from .preprocessor import TrapImagePreprocessor, PreprocessorConfig
-from .region_proposer import RegionProposer, RegionProposerConfig
-from .feature_filter import FeatureFilter, FeatureFilterConfig
-
-from .pipeline import SLFDetectionPipeline, PipelineResult, Detection
-
-__all__ = [
-    "TrapImagePreprocessor", "PreprocessorConfig",
-    "RegionProposer", "RegionProposerConfig",
-    "FeatureFilter", "FeatureFilterConfig",
-    "SLFDetectionPipeline", "PipelineResult", "Detection",
-    "DescriptorClassifier", "DescriptorClassifierConfig",
-]
-
-from .descriptor_classifier import DescriptorClassifier, DescriptorClassifierConfig
+__all__ = []
